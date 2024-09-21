d1214df13884dd2ef1fea8b33c38de29c8506696
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-09-21 23:30:01 +0300

    feat(web): add primevue

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
