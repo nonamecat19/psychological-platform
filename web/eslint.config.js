3174805f530f4fecc8e6afbb8a0d9ad261f7795a
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-10-18 22:53:54 +0300

    chore(global): vite and eslint configs

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -2,8 +2,10 @@ import vue from 'eslint-plugin-vue'
 import typescript from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import vueParser from 'vue-eslint-parser'
+import pluginVue from 'eslint-plugin-vue'
 
 export default [
+  ...pluginVue.configs['flat/recommended'],
   {
     files: ['**/*.ts', '**/*.js', '**/*.vue'],
     ignores: ['node_modules', 'dist'],
@@ -31,6 +33,7 @@ export default [
       'no-unused-vars': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
       'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off'
     },
   },
   {
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,11 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// @ts-expect-error
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslint()],
   resolve: {
     alias: [
       {
